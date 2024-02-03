f49f601dba19e8aa95641348f682c39c93cd0f3e
Mariano Barros <marianobarros@protonmail.com>
2024-02-03 14:50:08 +0100

    Chapter 5 - TDD Part 2: Finish & Refactor

--- tests/Unit/Entity/DinosaurTest.php
+++ tests/Unit/Entity/DinosaurTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class DinosaurTest extends TestCase
 {
-    public function testCangetAndSetData(): void
+    public function testCanGetAndSetData(): void
     {
         $dino = new Dinosaur(
             name: 'Big Eaty',
@@ -20,5 +20,26 @@ class DinosaurTest extends TestCase
         self::assertSame('Tyrannosaurus', $dino->getGenus());
         self::assertSame(15, $dino->getLength());
         self::assertSame('Paddock A', $dino->getEnclosure());
+    }
+
+    public function testDino10MetersOrGreaterIsLarge(): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: 10);
+
+        self::assertSame('Large', $dino->getSizeDescription(), 'This is supposed to be a large Dinosaur');
+    }
+
+    public function testDinoBetween5And9MetersIsMedium(): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: 5);
+
+        self::assertSame('Medium', $dino->getSizeDescription(), 'This is supposed to be a medium Dinosaur');
+    }
+
+    public function testDinoUnder5MetersIsSmall(): void
+    {
+        $dino = new Dinosaur(name: 'Big Eaty', length: 4);
+
+        self::assertSame('Small', $dino->getSizeDescription(), 'This is supposed to be a small Dinosaur');
     }
 }
